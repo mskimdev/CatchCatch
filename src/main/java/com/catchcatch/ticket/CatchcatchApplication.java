9a51a385d8d70e2d6bcf6af173aea46eefc90f09
mskimdev <aksen1754@naver.com>
2026-05-27 17:24:25 +0900

    first commit

---
+++ src/main/java/com/catchcatch/ticket/CatchcatchApplication.java
@@ -0,0 +1,13 @@
+package com.catchcatch.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchcatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatchcatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/catchcatch/ticket/CatchcatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catchcatch.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchcatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
