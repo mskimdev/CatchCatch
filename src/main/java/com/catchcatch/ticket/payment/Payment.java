812b40be56c34bd9fadc22e0b3ee84353ec90f13
unknown <dkswnsgus66@gmail.com>
2026-05-28 14:17:33 +0900

    create PaymentDTO

---
+++ src/main/java/com/catchcatch/ticket/payment/Payment.java
@@ -0,0 +1,76 @@
+package com.catchcatch.ticket.payment;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@Table(name = "payment_tb")
+@Entity
+public class Payment {
+
+
+//    컬럼명	타입	키	NOT NULL	기본값	설명
+//    id	INT (PK, AI)	PK	Y	AUTO_INCREMENT	결제 고유 ID
+//    booking_id	INT	FK(booking)	Y		예매 ID (booking_tb.id 참조)
+//    user_id	INT	FK(user)	Y		사용자 ID (user_tb.id 참조)
+//    imp_uid	VARCHAR(100)		Y		포트원 결제 고유 번호
+//    merchant_uid	VARCHAR(100)		Y		가맹점 주문 번호 (UNIQUE)
+//    amount	INT		Y		결제 금액 (원)
+//    method	VARCHAR(30)		Y		결제 수단 (card / kakaopay / tosspay)
+//    status	VARCHAR(20)		Y	READY	READY / PAID / CANCELLED
+//    paid_at	TIMESTAMP		N	NULL	결제 완료 일시
+//    created_at	TIMESTAMP		Y	CURRENT_TIMESTAMP	결제 요청 일시
+
+
+    // 결제 고유 ID
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Integer Id;
+
+    // 예매 ID (booking_tb.id 참조)
+    @OneToOne(fetch = FetchType.LAZY) // CatchCatch 명세서상 1:1 관계 (성능을 위해 LAZY 권장)
+    @JoinColumn(name = "booking_id", nullable = false) // FK 컬럼명 지정 및 NOT NULL 설정
+    private Booking booking; // Integer 대신 연관된 Booking 엔티티 객체를 직접 참조
+
+    // 사용자 ID (user_tb.id 참조)
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    // 포트원 결제 고유 번호
+    @Column(nullable = false)
+    private String impUid;
+
+    // 가맹점 주문 번호 (UNIQUE)
+    @Column(nullable = false,unique = true)
+    private String merchant_uid;
+
+    // 결제 금액 (원)
+    @Column(nullable = false)
+    private Integer amount;
+
+    // 결제 수단 (card / kakaopay / tosspay)
+    @Column(nullable = false)
+    private String method;
+
+    // READY / PAID / CANCELLED
+    @ColumnDefault("READY")
+    @Column(nullable = false)
+    private String status;
+
+    // 결제 완료 일시
+    @Column(nullable = true)
+    private Timestamp paidAt;
+
+    // 결제 요청 일시
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+}
---
+++ src/main/java/com/catchcatch/ticket/refound/Refound.java
@@ -0,0 +1,4 @@
+package com.catchcatch.ticket.refound;
+
+public class Refound {
+}
