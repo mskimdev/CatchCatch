f359511904fcfa9e2e2975dc34428467c3534e74
githoeuk <dnrdlwlsdl@gmail.com>
2026-06-02 15:36:30 +0900

    refactor: concertList-link available

--- src/main/java/com/catchcatch/ticket/refund/RefundService.java
+++ src/main/java/com/catchcatch/ticket/refund/RefundService.java
@@ -14,53 +14,53 @@ import org.springframework.transaction.annotation.Transactional;
 public class RefundService {
 
     private final RefundRepository refundRepository;
-    private final PaymentRepository paymentRepository;
+   // private final PaymentRepository paymentRepository;
 
     /**
      * 환불 처리
      * booking -> payment -> refund 라서
      * 결제내역ID 만 사용할 예정 (TODO - Booking 삭제)
      */
-    @Transactional
-    public RefundResponse.DetailDTO refundProc(RefundRequest.SaveDTO reqDTO) {
-
-        Payment payment = paymentRepository.findById(reqDTO.getPaymentId()).orElseThrow(
-                () -> new BadRequestException("결제 내역을 찾을 수 없습니다."));
-
-        if (refundRepository.existsByPayment_Id(reqDTO.getPaymentId())) {
-            throw new BadRequestException("이미 환불 처리된 결제입니다.");
-        }
-
-        Integer cancelFee = calculateRefundFee(payment);
-        Integer refundPrice = payment.getAmount() - cancelFee;
-
-        if(refundPrice < 0) {
-            refundPrice = 0;
-        }
-
-        String refundReason = reqDTO.getReason();
-
-        Refund refund = Refund
-                .builder()
-                .payment(payment)
-                .refundPrice(refundPrice)
-                .cancelFee(cancelFee)
-                .refundReason(refundReason)
-                .build();
-
-        Refund savedRefund = refundRepository.save(refund);
-
-        // TODO: 결제 상태 변경
-        // payment.refund();
-
-        // TODO: 예매 상태 변경
-        // booking.cancel();
-
-        // TODO: 좌석 상태 복구
-        // seatService.좌석해제(...);
-
-        return new RefundResponse.DetailDTO(savedRefund);
-    }
+//    @Transactional
+//    public RefundResponse.DetailDTO refundProc(RefundRequest.SaveDTO reqDTO) {
+//
+//        Payment payment = paymentRepository.findById(reqDTO.getPaymentId()).orElseThrow(
+//                () -> new BadRequestException("결제 내역을 찾을 수 없습니다."));
+//
+//        if (refundRepository.existsByPayment_Id(reqDTO.getPaymentId())) {
+//            throw new BadRequestException("이미 환불 처리된 결제입니다.");
+//        }
+//
+//        Integer cancelFee = calculateRefundFee(payment);
+//        Integer refundPrice = payment.getAmount() - cancelFee;
+//
+//        if(refundPrice < 0) {
+//            refundPrice = 0;
+//        }
+//
+//        String refundReason = reqDTO.getReason();
+//
+//        Refund refund = Refund
+//                .builder()
+//                .payment(payment)
+//                .refundPrice(refundPrice)
+//                .cancelFee(cancelFee)
+//                .refundReason(refundReason)
+//                .build();
+//
+//        Refund savedRefund = refundRepository.save(refund);
+//
+//        // TODO: 결제 상태 변경
+//        // payment.refund();
+//
+//        // TODO: 예매 상태 변경
+//        // booking.cancel();
+//
+//        // TODO: 좌석 상태 복구
+//        // seatService.좌석해제(...);
+//
+//        return new RefundResponse.DetailDTO(savedRefund);
+//    }
 
     /**
      * 환불 수수료 계산
