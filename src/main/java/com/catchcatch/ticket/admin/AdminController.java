f5d2e774a640811132f554c4471e0afdb9f85534
githoeuk <dnrdlwlsdl@gmail.com>
2026-06-08 17:20:35 +0900

    refactor: templates/concert/create.mustache 인터í페이스 수정

--- src/main/java/com/catchcatch/ticket/admin/AdminController.java
+++ src/main/java/com/catchcatch/ticket/admin/AdminController.java
@@ -3,12 +3,7 @@ package com.catchcatch.ticket.admin;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/admin")
