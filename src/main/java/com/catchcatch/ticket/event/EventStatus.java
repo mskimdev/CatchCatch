5b5d5c722bd7d99262253ae1eef0145a12ebbda9
siuuuuuhun <kimsihun0928@naver.com>
2026-06-11 17:19:16 +0900

    feat: event, eventHistory, point 엔티티 추가

---
+++ src/main/java/com/catchcatch/ticket/event/EventStatus.java
@@ -0,0 +1,7 @@
+package com.catchcatch.ticket.event;
+
+public enum EventStatus {
+    ACTIVE,
+    ENDED,
+    HIDDEN
+}
---
+++ src/main/java/com/catchcatch/ticket/eventhistory/EventHistory.java
@@ -0,0 +1,43 @@
+package com.catchcatch.ticket.eventhistory;
+
+import com.catchcatch.ticket.event.Event;
+import com.catchcatch.ticket.user.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "event_history_tb")
+public class EventHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    // 이벤트 참여 사용자
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    // 참여한 이벤트
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+    @CreationTimestamp
+    @Column(name = "participated_at", nullable = false, updatable = false)
+    private Timestamp participatedAt;
+
+    @Builder
+    public EventHistory(User user, Event event) {
+        this.user = user;
+        this.event = event;
+    }
+}
