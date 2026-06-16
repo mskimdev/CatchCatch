ba8342015bd2d343c8dc3c1e44a0a34ab73958d0
tancheong <jeong101887@gmail.com>
2026-06-16 16:45:36 +0900

    feat: admin 공지사항 등록 구현

--- src/main/java/com/catchcatch/ticket/notice/NoticeRequest.java
+++ src/main/java/com/catchcatch/ticket/notice/NoticeRequest.java
@@ -1,9 +1,29 @@
 package com.catchcatch.ticket.notice;
 
 import com.catchcatch.ticket.core.util.DateUtil;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 public class NoticeRequest {
+    public record SaveDTO(
+            String isPinned,
 
+            @NotBlank(message = "제목을 입력해주세요")
+            @Size(max = 50, message = "제목은 50자 이하로 입력해주세요")
+            String title,
+
+            @NotBlank(message = "내용을 입력해주세요")
+            String content
+    ) {
+        public Notice toEntity() {
+            return Notice.builder()
+                    .isPinned(isPinned != null && isPinned.equals("true") ? true : false)
+                    .title(title)
+                    .content(content)
+                    .build();
+        }
+    }
 
 }
