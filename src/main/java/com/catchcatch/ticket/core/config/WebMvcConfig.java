a245856f3172921cfe00d1390309ba04ac1f58fa
KIMMINSUuu <aksen1754@naver.com>
2026-06-25 01:20:54 +0900

    fix: WebMvcConfig 오타 수정

--- src/main/java/com/catchcatch/ticket/core/config/WebMvcConfig.java
+++ src/main/java/com/catchcatch/ticket/core/config/WebMvcConfig.java
@@ -54,7 +54,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 )
                 .excludePathPatterns(
                         "/api/concert/*/reviews",
-                        "/api/concert/*/reviews/*"
+                        "/api/concert/*/reviews/*",
                         "/api/concert/**",
                         "/api/queue/admin/**"
                 )
