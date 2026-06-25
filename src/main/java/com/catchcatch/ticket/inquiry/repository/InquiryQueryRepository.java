e57450c1a6b89aeb4e7a685d3374c60b4367a020
mskimdev <aksen1754@naver.com>
2026-06-25 14:10:12 +0900

    refactor: InquiryQueryRepository에 인터페이스 분리로 DIP 적용

--- src/main/java/com/catchcatch/ticket/inquiry/repository/InquiryQueryRepository.java
+++ src/main/java/com/catchcatch/ticket/inquiry/repository/InquiryQueryRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
-public class InquiryQueryRepository {
+public class InquiryQueryRepository implements InquiryRepositoryCustom{
 
     private final EntityManager em;
 
